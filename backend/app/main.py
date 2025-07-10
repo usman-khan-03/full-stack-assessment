99844a21be5552bc5b65c83e58473fea8113811a
Usman Khan <muhukhan@umass.edu>
2025-07-10 05:49:41 -0400

    deployed front end link in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",     # local frontend
-        "https://boxsy-frontend.vercel.app"  # live frontend
+        "https://aisle-front-end.vercel.app/"  # live frontend
     ],
     allow_credentials=True,
     allow_methods=["*"],
